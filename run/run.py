801120da05e5a9ae657eea33e98b628fa60b0def
Christian Sonnabend <sonnabendch@gmail.com>
2025-12-12 14:57:52 +0100

    Adding multi-config support (#31)

--- run/run.py
+++ run/run.py
@@ -1,67 +1,86 @@
-import sys, os, json, subprocess
+import sys, os, json, subprocess, glob
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--config", default="configuration.json", help="Path to the configuration file")
+parser.add_argument("-skip-q", "--skip-question", type=int, default=0, help="Skip the confirmation question to proceed with the run (1 to skip, 0 to ask)")
 args = parser.parse_args()
 
-if not os.path.exists(args.config):
-    print(f"Configuration file {args.config} not found. Aborting")
-    sys.exit(1)
+if "*" in args.config:
+    args.config = glob.glob(args.config, recursive=True)
+else:
+    args.config = [os.path.abspath(args.config)]
 
-with open(args.config, 'r') as config_file:
-    CONFIG = json.load(config_file)
-sys.path.append(CONFIG['settings']['framework'] + "/framework")
-from base import *
+framework_path = ""
+for i, config_file in enumerate(args.config):
+    
+    if not os.path.isfile(config_file):
+        print(f"Configuration file {config_file} not found. Aborting")
+        sys.exit(1)
+        
+    with open(config_file, 'r') as cf:
+        CONFIG = json.load(cf)
+    
+    current_framework_path = CONFIG['settings']['framework'] + "/framework"
+    framework_changed = False
+    if i == 0 or framework_path != current_framework_path:
+        if i == 0 and framework_path != "":
+            sys.path.remove(framework_path)
+        framework_path = CONFIG['settings']['framework'] + "/framework"
+        framework_changed = True
 
-LOG = logger("Framework")
-LOG.welcome_message()
+    if framework_changed:
+        sys.path.append(framework_path)
+        from base import *
 
-fetch_upstream("origin", path=CONFIG['settings']['framework'])
-fetch_upstream("upstream", path=CONFIG['settings']['framework'])
-if CONFIG['settings'].get('git', {}).get('checkout', 0) == 1:
-    LOG.framework("Checking out the specified git commit/tag/branch...")
-    git_config = CONFIG['settings']['git']
-    checkout_from_config(
-        git_config=git_config,
-        path=CONFIG['settings']['framework']
-    )
-full_git_config(verbose=True, path=CONFIG['settings']['framework'])
+        LOG = logger("Framework")
+        LOG.welcome_message()
 
-CONFIG = add_name_and_path(CONFIG)
-can_we_continue() # Ask user if they want to continue with the given configuration, recreates the output folders if necessary
-create_folders(CONFIG)
-config_path = copy_config(CONFIG)
+        fetch_upstream("origin", path=CONFIG['settings']['framework'])
+        fetch_upstream("upstream", path=CONFIG['settings']['framework'])
+        
+    if CONFIG['settings'].get('git', {}).get('checkout', 0) == 1:
+        LOG.framework("Checking out the specified git commit/tag/branch...")
+        git_config = CONFIG['settings']['git']
+        checkout_from_config(
+            git_config=git_config,
+            path=CONFIG['settings']['framework']
+        )
+    full_git_config(verbose=True, path=CONFIG['settings']['framework'])
 
-masterjob_defaults = {
-    "partition": "main",
-    "time": "60",
-    "mem": "150G",
-    "cpus-per-task": "16"
-}
+    CONFIG = add_name_and_path(CONFIG)
+    if args.skip_question == 0:
+        can_we_continue() # Ask user if they want to continue with the given configuration, recreates the output folders if necessary
+    create_folders(CONFIG)
+    config_path = copy_config(CONFIG)
 
-deep_update(masterjob_defaults, CONFIG.get('masterjob', {}), name="Masterjob settings", verbose=False)
-masterjob_defaults["framework_path"] = CONFIG['settings']['framework']
-masterjob_defaults["output_path"] = CONFIG["output"]["general"]["path"]
+    masterjob_defaults = {
+        "partition": "main",
+        "time": "60",
+        "mem": "150G"
+    }
 
-masterjob_defaults = replace_in_dict_keys(masterjob_defaults, '-', '_')
+    deep_update(masterjob_defaults, CONFIG.get('masterjob', {}), name="Masterjob settings", verbose=False)
+    masterjob_defaults["framework_path"] = CONFIG['settings']['framework']
+    masterjob_defaults["output_path"] = CONFIG["output"]["general"]["path"]
 
-exec_script = f"""#!/bin/bash
+    masterjob_defaults = replace_in_dict_keys(masterjob_defaults, '-', '_')
+
+    exec_script = f"""#!/bin/bash
 #SBATCH --job-name=TPCPID_MASTERJOB
 #SBATCH --chdir={masterjob_defaults['framework_path']}
 #SBATCH --time={masterjob_defaults['time']}
 #SBATCH --mem={masterjob_defaults['mem']}
 #SBATCH --partition={masterjob_defaults['partition']}
-#SBATCH --cpus-per-task={masterjob_defaults['cpus_per_task']}
 #SBATCH --output={masterjob_defaults['output_path']}/run_%j.out
 #SBATCH --error={masterjob_defaults['output_path']}/run_%j.err
 
 time python3 {masterjob_defaults['framework_path']}/run/src/run_framework.py --config $1
-"""
+    """
 
-script_path = os.path.join(masterjob_defaults["framework_path"], 'run/src/RUN_SLURM.sh')
-with open(script_path, 'w') as script_file:
-    script_file.write(exec_script)
+    script_path = os.path.join(masterjob_defaults["framework_path"], 'run/src/RUN_SLURM.sh')
+    with open(script_path, 'w') as script_file:
+        script_file.write(exec_script)
 
-slurm_out = subprocess.check_output(f"sbatch {script_path} {config_path}", shell=True).decode().strip('\n')
-LOG.framework(f"TPCPID_MASTERJOB job submitted successfully. Job ID: {slurm_out.split()[-1]}")
+    slurm_out = subprocess.check_output(f"sbatch {script_path} {config_path}", shell=True).decode().strip('\n')
+    LOG.framework(f"TPCPID_MASTERJOB job submitted successfully. Job ID: {slurm_out.split()[-1]}")
