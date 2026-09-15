4a499e0838e717841f0b10ac7dd9e7de6e20d53b
Christian Sonnabend <sonnabendch@gmail.com>
2026-09-15 14:53:08 +0200

    Fix

--- run/ci/o2physics_pid_test.py
+++ run/ci/o2physics_pid_test.py
@@ -181,7 +181,8 @@ def main():
     log_file = workdir / "o2physics_pid.log"
     write_config(config, aod_file, onnx_file)
 
-    option = f"-b --configuration json://{shlex.quote(str(config))} --aod-memory-rate-limit 2000000000 --shm-segment-size 4000000000 --resources-monitoring 0 --min-failure-level error"
+    shm_segment_size = 536870912
+    option = f"-b --configuration json://{shlex.quote(str(config))} --aod-memory-rate-limit 2000000000 --shm-segment-size {shm_segment_size} --resources-monitoring 0 --min-failure-level error"
     command = (
         f"o2-analysis-event-selection-service {option} --aod-file {shlex.quote(str(aod_file))} | "
         f"o2-analysis-pid-tpc-service {option}"
